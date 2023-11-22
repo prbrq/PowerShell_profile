6131b1f6502e21a5cbf0d7ce1fc929ef768fcaa2
Аркадий <chelyadnikov@kn-k.ru>
2023-11-22 17:12:37 +0300

    Added a message on successful UUID generation

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
-function GenerateUUIDv4 {
-  Set-Clipboard (New-Guid).ToString()
-}
 Set-Alias -Name "guc" -Value GenerateUUIDv4 # Generate a Uuid to the Clipboard
+
+function GenerateUUIDv4 {
+  $uuid = (New-Guid).ToString()
+  Set-Clipboard $uuid
+  Write-Host "The UUID `"$uuid`" was copied to the clipboard."
+}
